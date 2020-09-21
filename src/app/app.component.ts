a1cef9d5d3f69353b1476d6024a5e8d5f6a9a37b
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2020-09-21 12:46:36 +0300

    setup linter for clr migration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+    template: `
+        <button class="btn btn-primary">Le button</button>
+    `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
